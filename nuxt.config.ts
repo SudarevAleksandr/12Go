bcd06019fd5c08d3b4172a1fe42b37afdc875609
Sudarev Aleksandr <sudarev_aleksandr@mail.ru>
2024-06-03 16:58:56 +0300

    update_deploy_actionsgit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   buildDir: 'dist',
-
+  app: {
+    baseURL: '/12Go/', // Добавляем префикс ко всем ссылкам
+    //baseURL: '/.output/public/', // Добавляем префикс ко всем ссылкам
+  },
+  nitro: {
+    prerender: {
+      routes: ['/'], // Указываем главную страницу для предварительного рендеринга
+    },
+    output: {
+      dir: '.output', // Указываем директорию для вывода сборки
+    },
+  },
   devtools: { enabled: true },
   modules: ['dayjs-nuxt'],
 })
